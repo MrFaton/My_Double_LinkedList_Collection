c21808bc399e1c5bc19541743269aac83846ab75
MrFaton <firefly90@inbox.ru>
2014-08-18 11:04:12 +0300

    update from work

--- src/MyDoubleLinkedListCollection.java
+++ src/MyDoubleLinkedListCollection.java
@@ -9,7 +9,93 @@ public class MyDoubleLinkedListCollection {
         makeList(1, 2, 3, 6, 8, 10, 12);
 
         System.out.println("Основной список: "+printToString(doubleListTail)); //значки "<-  ->"обозначают ссылку, например 1<-(2)->3, значит что у элемента с индексом 1 и значением 2 предыдущий элемент со значением 1, а сдедующий элемент со значением 3
+//        System.out.println("Добавить значение в 'голову': "+printToString(addToHead(20, doubleListTail)));
+//        System.out.println("Добавить значение в 'голову' итеративно: "+printToString(addToHeadIter(21, doubleListTail)));
+//        System.out.println("Добавить значение в 'голову' рекурсивно: "+printToString(addToHeadRec(22, doubleListTail)));
+//        System.out.println("Удалить 'голову': "+printToString(removeHead(doubleListTail)));
+//        System.out.println("Удалить 'голову' итеративно: "+printToString(removeHeadIter(doubleListTail)));
+//        System.out.println("Удалить 'голову' рекурсивно: "+printToString(removeHeadRec(doubleListTail)));
+        System.out.println("Вставить в позицию итеративно: "+printToString(inseartInPositionIter(30, 3, doubleListTail)));
     }
+
+    public static DoubleNode addToHead(int value, DoubleNode list){
+        doubleListHead.next=new DoubleNode(value, doubleListHead, null);
+        doubleListHead=doubleListHead.next;
+        return list;
+    }
+
+    public static DoubleNode addToHeadIter(int value, DoubleNode list){
+        DoubleNode currentItemIter=list;
+        while (currentItemIter.next!=null){
+            currentItemIter=currentItemIter.next;
+        }
+        currentItemIter.next=new DoubleNode(value, currentItemIter, null);
+        doubleListHead=currentItemIter.next;
+        return list;
+    }
+
+    public static DoubleNode addToHeadRec(int value, DoubleNode list){
+        if(list.next!=null){
+            addToHeadRec(value, list.next);
+        } else {
+            list.next=new DoubleNode(value, list, null);
+            doubleListHead=list.next;
+        }
+        return doubleListTail;
+    }
+
+    public static DoubleNode removeHead(DoubleNode list){
+        doubleListHead=doubleListHead.previous;
+        doubleListHead.next=null;
+        return list;
+    }
+
+    public static DoubleNode removeHeadIter(DoubleNode list){
+        DoubleNode currentItemIter=list;
+        while (currentItemIter.next!=null){
+            currentItemIter=currentItemIter.next;
+        }
+        currentItemIter=currentItemIter.previous;
+        currentItemIter.next=null;
+        doubleListHead=currentItemIter;
+        return list;
+    }
+
+    public static DoubleNode removeHeadRec(DoubleNode list){
+        if (list.next!=null){
+            removeHeadRec(list.next);
+        } else {
+            list=list.previous;
+            list.next=null;
+            doubleListHead=list;
+        }
+        return doubleListTail;
+    }
+
+    public static DoubleNode inseartInPositionIter(int value, int position, DoubleNode list){
+        DoubleNode currentItemIter=list;
+        DoubleNode itemToInseart=new DoubleNode(value, null, null);
+        if(position==0) {
+            list.previous=itemToInseart;
+            itemToInseart.next=list;
+            list=itemToInseart;
+        } else {
+            int i=0;
+            while (i<position){
+                currentItemIter=currentItemIter.next;
+                i++;
+            }
+            itemToInseart.previous=currentItemIter.previous;
+            currentItemIter=currentItemIter.previous;
+            itemToInseart.next=currentItemIter.next;
+            currentItemIter.next=itemToInseart;
+            currentItemIter=currentItemIter.next.next;
+            currentItemIter.previous=itemToInseart;
+        }
+        return list;
+    }
+
+    // Внутренние методы...
 
     public static DoubleNode makeList(int... values) {
         for (int i = values.length - 1; i >= 0; i--) {
